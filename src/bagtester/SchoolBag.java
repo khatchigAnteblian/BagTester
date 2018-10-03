84a57bdcc2bea81deae93c83b1c4434a26d4e352
Khatchig Anteblian <Khatchig.Anteblian@student.tdsb.on.ca>
2018-10-03 13:49:29 -0400

    Added instance fields and constructors for SchoolBag class

--- src/bagtester/SchoolBag.java
+++ src/bagtester/SchoolBag.java
@@ -11,4 +11,20 @@ package bagtester;
  */
 public class SchoolBag {
     
+    public static final int bagWeight = 600; // In grams
+    
+    private String style;
+    private Binder[] binders = new Binder[2];
+    
+    public SchoolBag(String style, Binder binder1, Binder binder2) {
+        this.style = style;
+        this.binders[0] = binder1;
+        this.binders[1] = binder2;
+    }
+    
+    public SchoolBag(String style, String lbl1, String lbl2, String clr1, String clr2, int num1, int num2) {
+        this.style = style;
+        this.binders[0] = new Binder(lbl1, clr1, num1);
+        this.binders[1] = new Binder(lbl2, clr2, num2);
+    }
 }
